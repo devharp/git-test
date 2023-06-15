bdf7f4778430b253fb6c8381b4cbbe31ff0f6952
vaibhavingale555 <vaibhavingale55@gmail.com>
2023-06-15 11:17:53 +0530

    Added a 'addition' function

--- prog.js
+++ prog.js
@@ -18,5 +18,16 @@ function subtract(x, y) {
   return x - y;
 }
 
+/**
+ * Function is used to perform addition operation
+ * 
+ * @param { number } x - First number 
+ * @param { number } y - Second number
+ * @returns { number } - Returns a addition of two numbers
+ */
+function add(x,y) {
+  return x+y;
+}
+
 // Call the main function to initialize the application
 main();
